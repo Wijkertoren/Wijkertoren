e8cb2516fa707cd89f6da71ec96d7c45014cf786
DarknesssX <meyer.jeffrey@hotmail.nl>
2017-02-07 09:38:39 +0100

    Revert

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -10,7 +16,6 @@
         {
             echo "noot";
             echo "woop woop";
-            //hoi mezelf
         }
         ?>
     </body>
