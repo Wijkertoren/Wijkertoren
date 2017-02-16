a3c1d000c96d863fff4c9ed2a333d5ad7e188e8e
DarknesssX <meyer.jeffrey@hotmail.nl>
2017-02-16 13:26:40 +0100

    Changed Code Comment Trying to create a merge conflict after pull request

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <title>Wijkertoren Ledenregister</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <title>Test Conflict merging</title>
     </head>
     <body>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
-        
-        #Our Code Goes Here Capitals.       
+        <?php
+            $a = 4;
+            if ($a === 4)
+            {
+                echo "Merge conflict?";
+            }
+        ?>
     </body>
 </html>
+
