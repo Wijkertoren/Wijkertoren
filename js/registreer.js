156fdcfdc301d1aa800da2e7c7e7194f08c66d5c
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-03-21 14:26:07 +0100

    Fixed the buttons:
    fixed the button because it was not working now it executes the script good 
    
    fixes:
    - changed form ID to the corresponding id to the scripts ID
    - Changed the position of the form because the button was not in the form thats why it didn't want to work :)

--- js/registreer.js
+++ js/registreer.js
@@ -1,9 +1,30 @@
 $(document).ready(function () {
-    $('#2DB').on('submit', function(e){
-        e.preventDefault;
+    $('#2DB').on('submit', function (e) {
+        e.preventDefault();
         
-        $.ajax(function() {
-            method: "POST"
+        var data = {
+            Voornaam: $('#Voornaam').val(),
+            Achternaam: $('#Achternaam').val(),
+            Tussenvoegsel: $('#Tussenvoegsel').val(),
+            Geslacht: $('#Geslacht').val(),
+            Organisatie: $('#Organisatie').val(),
+            ContactPersoon: $('#Contact_persoon').val(),
+            Email: $('#Email').val(),
+            ExtraEmail: $('#Extra_email').val(),
+            Telefoon: $('#Telefoon').val(),
+            Mobiel: $('#Mobiel').val(),
+            Adres: $('#Adres').val(),
+            Woonplaats: $('#Woonplaats').val(),
+            Postcode: $('#Postcode').val(),
+            ExtraInfo: $('#Extra_info').val()
+        };
+
+        $.ajax({
+            method: "POST",
+            url: 'EXPORT2Database.php',
+            data: data
+        }).done(function (data) {
+            alert(data);
         });
     });
 });
