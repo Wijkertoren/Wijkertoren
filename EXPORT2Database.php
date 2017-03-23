6d3124b3050043f1d401083c2fdcf2b9a0bd3fa5
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-03-23 10:15:07 +0100

    JSON functie aangemaakt om de data in opteslaan.
    todo:
    - decode om door te geven naar de database 
    - omzetten naar een Query na het decoden.

--- EXPORT2Database.php
+++ EXPORT2Database.php
@@ -1,5 +1,14 @@
 <?php
+//creates an stdClass to view values given below.
+$formValues = new stdClass();
 foreach ($_POST as $key => $value)
 {
-    echo $key . ": " . $value . "</br>";
+    //creates variable that loops through the $_POST to get the data given with the input field.
+    $formValues->$key = $value;
+    
+    //encodes the values given in the input field and puts them in JSON format.
+    $encodeFormJSON = json_encode($formValues);
+    
+    //$decodeJSON = json_decode($encodeFormJSON);
 }
+echo $encodeFormJSON;
