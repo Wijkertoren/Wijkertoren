7ce9df4b6149fc63c53dd552f054629314ad1b05
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-04-13 12:13:09 +0200

    Fixed half of the problems fixing Deleting Members in progress.

--- js/showlid.js
+++ js/showlid.js
@@ -3,16 +3,38 @@ $(document).ready(function () {
     $('#LidModal').click(function (e) {
         //Haalt het lid id op van het geselecteerde lid.
         var data = {
-            lid_ID: $('.selected').data('id')
+            lid_ID: $('.selected').data('id'),
+            persoon_ID: $('.selected').data('persoonnr'),
+            organisatie_ID: $('.selected').data('organisatienr')
         };
+
+        console.log(data);
 
         //Voert het geselecteerde lid id door naar "ShowMember.php".
         $.ajax({
-            method: "POST",
+            method: "GET",
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
             url: 'script/ShowMember.php',
             data: data
-        }).done(function (data) {
+        }).done(function (test) {
+            var Array = test;
 
+            $('#VoornaamFromDB').val(Array["Voornaam"]);
+            $('#AchternaamFromDB').val(Array["Achternaam"]);
+            $('#TussenvoegselFromDB').val(Array["Tussenvoegsel"]);
+            $('#GeslachtFromDB').val(Array["Geslacht"]);
+            $('#OrganisatieFromDB').val(Array["Organisatie_naam"]);
+            $('#Contact_persoonFromDB').val(Array["Contact_persoon"]);
+            $('#EmailFromDB').val(Array["Email"]);
+            $('#Extra_emailFromDB').val(Array["Extra_email"]);
+            $('#TelefoonFromDB').val(Array["Telefoon"]);
+            $('#MobielFromDB').val(Array["Mobiel"]);
+            $('#AdresFromDB').val(Array["Adres"]);
+            $('#WoonplaatsFromDB').val(Array["Woonplaats"]);
+            $('#PostcodeFromDB').val(Array["Postcode"]);
+            $('#ZoektermenFromDB').val(Array["Extra_info"]);
+            $('#Donatie_kenmerkFromDBa').val(Array["Donatie_kenmerk"]);
         });
     });
 });
